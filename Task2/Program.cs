a5843ddaccb3f5dd2b8e70d930b14c0a528fdb05
Your Name <you@example.com>
2022-08-26 22:39:40 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+Console.WriteLine ("Введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+string st = number.ToString();
+if (st.Length >= 3) {
+    Console.WriteLine (st[2]);
+    }
+    else {
+        Console.WriteLine ("no 3d number");
+    }
+
+//Console.WriteLine(st[1]);
